3976a166cc387285768c3c7150479b6a14dcb0f5
fanshuaiyao <1345822123@qq.com>
2025-10-01 13:15:35 +0800

    002-增删改

--- mybatis-002-crud/readme.md
+++ mybatis-002-crud/readme.md
@@ -6,3 +6,7 @@ u:update
 d:delete
 
 在JDBC中占位符是？ mybatis中是#{} 必须是这个
+且#{}中应该填写的是  **属性的get方法的方法名称去掉get**  eg：#{carNum}  -> getCarNum()
+严格意义来讲 #{}中 使用pojo类传递的话  大括号中应该写get方法去掉get然后将剩下的部分首字母小写 放进去
+
+mybatis 看到#{carNum} 然后进行拼接 getCarNum 然后通过通过**反射机制**访问这个get方法  拿到对象的carNum属性 
--- mybatis-002-crud/src/test/java/com/fan/mybatis/test/CarMapperTest.java
+++ mybatis-002-crud/src/test/java/com/fan/mybatis/test/CarMapperTest.java
@@ -1,10 +1,54 @@
 package com.fan.mybatis.test;
 
+import com.fan.mybatis.pojo.Car;
 import com.fan.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 public class CarMapperTest {
+    @Test
+    public void selectById(){
+        SqlSession sqlSession = SqlSessionUtil.openSqlsession();
+
+        sqlSession.selectOne("selectById",19);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateById(){
+        SqlSession sqlSession = SqlSessionUtil.openSqlsession();
+        Car car = new Car(19L, "4444", "audi7", 40.0, "2001-01-02", "氢能源车");
+
+
+        int updateById = sqlSession.update("updateById", car);
+        System.out.println(updateById);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteById(){
+        SqlSession sqlSession = SqlSessionUtil.openSqlsession();
+
+        int deleteById = sqlSession.delete("deleteById", 17);
+        System.out.println(deleteById);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void testInsertCarByPOJO(){
+        SqlSession sqlSession = SqlSessionUtil.openSqlsession();
+
+        Car car = new Car(null, "3333", "audi", 30.0, "200-01-02", "油车");
+        int count = sqlSession.insert("insertCar", car);
+        System.out.println(count);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
 
     @Test
     public void testInsertCar(){
