141d6089f7a6dc1270369e7c04eb290d14dfbdbc
fanshuaiyao <1345822123@qq.com>
2025-10-01 16:40:41 +0800

    003-数据源

--- mybatis-003-configuration/src/test/java/test/ConfigurationTest.java
+++ mybatis-003-configuration/src/test/java/test/ConfigurationTest.java
@@ -1,11 +1,41 @@
 package test;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
 public class ConfigurationTest {
+
+    @Test
+    public void testOutTime() throws Exception{
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 指定环境获取sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsReader("mybatis-config.xml"), "development");
+        for (int i = 0; i < 4; i++) {
+            SqlSession sqlSession1 = sqlSessionFactory.openSession();
+            int insertCar = sqlSession1.insert("insertCar");
+        }
+    }
+    @Test
+    public void testDataSource() throws Exception{
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 指定环境获取sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsReader("mybatis-config.xml"), "development");
+
+        // 会话一
+        SqlSession sqlSession1 = sqlSessionFactory.openSession();
+        int insertCar = sqlSession1.insert("insertCar");
+        sqlSession1.commit();
+        sqlSession1.close();
+
+        SqlSession sqlSession2 = sqlSessionFactory.openSession();
+        int insertCar1 = sqlSession2.insert("insertCar");
+        sqlSession2.commit();
+        sqlSession2.close();
+
+    }
 
     @Test
     public void testEnv() throws Exception {
