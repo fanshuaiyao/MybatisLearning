71a6ddfe210fe3698d83a47d8d62502f28250032
fanshuaiyao <1345822123@qq.com>
2025-10-01 15:44:10 +0800

    003-事务管理器

--- mybatis-003-configuration/src/test/java/test/ConfigurationTest.java
+++ mybatis-003-configuration/src/test/java/test/ConfigurationTest.java
@@ -1,11 +1,17 @@
 package test;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
 public class ConfigurationTest {
 
     @Test
-    public void testEnv(){
-        
+    public void testEnv() throws Exception {
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 指定环境获取sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsReader("mybatis-config,xml"), "default");
+
     }
 }
