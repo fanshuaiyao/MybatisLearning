8b30288c1789e88f24951b159e40813edfa4bc1f
fanshuaiyao <1345822123@qq.com>
2025-10-01 23:04:41 +0800

    godbatis-获取sql标签对象集合

--- godbatis/src/main/java/org/fan/core/SqlSessionFactoryBuilder.java
+++ godbatis/src/main/java/org/fan/core/SqlSessionFactoryBuilder.java
@@ -5,9 +5,11 @@ import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
+import org.fan.utils.Resources;
 
 import javax.sql.DataSource;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,6 +55,16 @@ public class SqlSessionFactoryBuilder {
             Element transactionEtl = environmentElt.element("transactionManager");
             Element dataSourceEtl = environmentElt.element("dataSource");
 
+            // 这个list集合存放着mapper的路径
+            List<String> sqlMapperXmlPathList = new ArrayList<>();
+            List<Node> list = document.selectNodes("//mapper");
+            list.forEach(node ->{
+                Element element = (Element) node;
+                String resource = element.attributeValue("resource");
+                sqlMapperXmlPathList.add(resource);
+            });
+
+
             // 获取数据源
             DataSource dataSource = getDataSource(dataSourceEtl);
 
@@ -60,7 +72,7 @@ public class SqlSessionFactoryBuilder {
             Transaction transaction = getTransaction(transactionEtl, dataSource);
 
             // 获取mappedStatements
-            Map<String, MappedStatement> mappedStatements = null;
+            Map<String, MappedStatement> mappedStatements = getMappedStatements(sqlMapperXmlPathList);
 
             // 解析文件  然后创建sqlSessionFactory对象
             sqlSessionFactory = new SqlSessionFactory(transaction, mappedStatements);
@@ -70,6 +82,52 @@ public class SqlSessionFactoryBuilder {
 
 
         return sqlSessionFactory;
+    }
+
+    /**
+     * 解析所有的mapper文件 构建mapper结合
+     * @param sqlMapperXmlPathList mapper文件路径集合
+     * @return 集合
+     */
+    private Map<String, MappedStatement> getMappedStatements(List<String> sqlMapperXmlPathList) {
+
+        // 存放所有的mapper文件中的sql语句标签
+        HashMap<String, MappedStatement> mappedStatementsMap = new HashMap<>();
+
+        sqlMapperXmlPathList.forEach(sqlMapperXmlPath -> {
+            try {
+                SAXReader reader = new SAXReader();
+                Document document = reader.read(Resources.getResourceAsStream(sqlMapperXmlPath));
+                // 开始解析mapper.xml文件
+                Element mapper = (Element) document.selectSingleNode("mapper");
+
+                // 获取namespace
+                String namespace = mapper.attributeValue("namespace");
+                // 获取所有的子节点
+                List<Element> elementList = mapper.elements();
+                elementList.forEach(element -> {
+                    String id = element.attributeValue("id");
+                    // 进行拼接 生成最终的sqlId
+                    String sqlId = namespace + "." + id;
+                    String resultType = element.attributeValue("resultType");
+                    // 获取sql语句
+                    String sql = element.getTextTrim();
+                    // 封装标签对象
+                    MappedStatement mappedStatement = new MappedStatement(resultType, sql);
+
+                    // 将封装好的标签对象放入map中
+                    mappedStatementsMap.put(sqlId, mappedStatement);
+                });
+
+            } catch (DocumentException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+
+
+        return mappedStatementsMap;
+
     }
 
     /**
